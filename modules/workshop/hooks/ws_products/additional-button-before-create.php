020d7941b61fbec7eb52436f3ae1ba71c8b49a54
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-13 17:34:19 +0700

    add import product

---
+++ modules/workshop/hooks/ws_products/additional-button-before-create.php
@@ -0,0 +1,35 @@
+<?php
+
+ob_start();
+
+?>
+<button class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Import</button>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Import Products or Services</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="<?=routeTo('workshop/products/import')?>" method="post" name="formImport" enctype="multipart/form-data">
+            <?= csrf_field() ?>
+            <div class="form-group">
+                <label for="">File</label>
+                <input type="file" name="file" id="" class="form-control">
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="formImport.submit()">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+
+$button = ob_get_clean();
+
+return $button;
---
+++ modules/workshop/process/products/import.php
@@ -0,0 +1,59 @@
+<?php
+
+use Core\Database;
+use Core\Request;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+if(Request::isMethod('POST'))
+{
+    $db = new Database;
+    $import = $_FILES['file'];
+    $allowedTypes = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+    if (!in_array($import['type'], $allowedTypes)) {
+        return redirectBack(['error'=> 'Silakan unggah file Excel']);
+    }
+    else
+    {
+        $fileExtension = pathinfo($import['name'], PATHINFO_EXTENSION);
+            
+        if (in_array($fileExtension, ['xls', 'xlsx'])) {
+            $spreadsheet = IOFactory::load($import['tmp_name']);
+            $sheet = $spreadsheet->getActiveSheet();
+    
+            foreach ($sheet->getRowIterator(2) as $row) {
+                $name = $sheet->getCell('B' . $row->getRowIndex())->getFormattedValue();
+                $unit = $sheet->getCell('C' . $row->getRowIndex())->getFormattedValue();
+                $category_name = $sheet->getCell('D' . $row->getRowIndex())->getFormattedValue();
+                $supplier = $sheet->getCell('E' . $row->getRowIndex())->getFormattedValue();
+                $price = $sheet->getCell('F' . $row->getRowIndex())->getFormattedValue();
+
+                $category = $db->single('ws_categories', [
+                    'name' => $category_name
+                ]);
+
+                if(!$category)
+                {
+                    $category = $db->insert('ws_categories', [
+                        'name' => $category_name
+                    ]);
+                }
+                
+                $product = $db->insert('ws_products', [
+                    'category_id' => $category->id,
+                    'name' => $name,
+                    'supplier_name' => $supplier,
+                    'record_type' => 'SPARE PART',
+                    'unit' => $unit
+                ]);
+
+                $db->insert('ws_product_prices', [
+                    'product_id' => $product->id,
+                    'amount' => (int) $price,
+                    'status' => "ACTIVE"
+                ]);
+            }
+        }
+    }
+
+    return redirectBack(['success' => 'Import Success']);
+}
