0e6f752a92fd5531daced4499f45ae2dac22061c
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-13 10:21:17 +0700

    update kolom harga

--- modules/workshop/hooks/ws_products/index.php
+++ modules/workshop/hooks/ws_products/index.php
@@ -17,7 +17,7 @@ if($filter)
 
 $where = $where ." ". $having;
 
-$query = "SELECT $this->table.*, ws_product_prices.amount price FROM $this->table INNER JOIN ws_product_prices ON ws_product_prices.product_id = $this->table.id AND ws_product_prices.status = 'ACTIVE' $where";
+$query = "SELECT $this->table.*, ws_product_prices.amount price FROM $this->table LEFT JOIN ws_product_prices ON ws_product_prices.product_id = $this->table.id AND ws_product_prices.status = 'ACTIVE' $where";
 $this->db->query = $query . " ORDER BY ".$col_order." ".$order[0]['dir']." LIMIT $start,$length";
 $data  = $this->db->exec('all');
 
