b4c1d414f1a25f5c3e5c1112f3d167fbec54d296
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-06-24 15:20:52 +0700

    update inspection

--- modules/workshop/assets/js/inspections.js
+++ modules/workshop/assets/js/inspections.js
@@ -14,5 +14,9 @@ $('select[name="ws_inspections[customer_id]"]').change(function(){
 })
 
 $('select[name="ws_inspections[customer_type]"]').change(function(){
-    window.location.href = '/crud/create?table=ws_inspections&customer_type='+$(this).val()
+    window.location.href = '/crud/create?table=ws_inspections&customer_type='+$(this).val()+'&vehicle_type='+$('select[name="ws_inspections[vehicle_type]"]')
+})
+
+$('select[name="ws_inspections[vehicle_type]"]').change(function(){
+    window.location.href = '/crud/create?table=ws_inspections&vehicle_type='+$(this).val()+'&customer_type='+$('select[name="ws_inspections[customer_type]"]')
 })
