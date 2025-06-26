ebe23f6de46de52328c4377664c64040876b2867
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-06-26 14:19:59 +0700

    fix existing customer

--- modules/workshop/hooks/ws_inspections/before-create.php
+++ modules/workshop/hooks/ws_inspections/before-create.php
@@ -5,26 +5,31 @@ use Core\Database;
 $db = new Database;
 $customer = $data['customer_id'];
 $vehicle = $data['vehicle_id'];
+
 try {
     // Ambil semua pasangan Key: Value
     preg_match_all('/^(.*?):\s*(.+)$/m', $customer, $customers, PREG_SET_ORDER);
 
-    // Buat array hasil
-    $result = [];
-
-    foreach ($customers as $match) {
-        $key = trim($match[1]);
-        $value = trim($match[2]);
-        $result[$key] = $value;
+    if($customers)
+    {
+        // Buat array hasil
+        $result = [];
+    
+        foreach ($customers as $match) {
+            $key = trim($match[1]);
+            $value = trim($match[2]);
+            $result[$key] = $value;
+        }
+    
+        $customer = $db->insert('ws_customers', [
+            'name' => $result['Nama'],
+            'address' => $result['Alamat'],
+            'phone' => $result['No. HP'],
+        ]);
+    
+        $data['customer_id'] = $customer->id;
     }
 
-    $customer = $db->insert('ws_customers', [
-        'name' => $result['Nama'],
-        'address' => $result['Alamat'],
-        'phone' => $result['No. HP'],
-    ]);
-
-    $data['customer_id'] = $customer->id;
 } catch (\Throwable $th) {
     //throw $th;
     // unset($data['customer_id']);
@@ -37,28 +42,32 @@ try {
     // Buat array hasil
     $result = [];
 
-    foreach ($vehicles as $match) {
-        $key = trim($match[1]);
-        $value = trim($match[2]);
-        $result[$key] = $value;
-    }
-
-    $dataKendaraan =  [
-        'name' => $result['Model'],
-        'merk' => $result['Merk'],
-        'type' => $result['Varian'],
-        'police_number' => $result['No. Polisi'],
-        'description' => $result['Tahun'],
-    ];
-
-    if(isset($data['customer_id']))
+    if($vehicles)
     {
-        $dataKendaraan['customer_id'] = $data['customer_id'];
+        foreach ($vehicles as $match) {
+            $key = trim($match[1]);
+            $value = trim($match[2]);
+            $result[$key] = $value;
+        }
+    
+        $dataKendaraan =  [
+            'name' => $result['Model'],
+            'merk' => $result['Merk'],
+            'type' => $result['Varian'],
+            'police_number' => $result['No. Polisi'],
+            'description' => $result['Tahun'],
+        ];
+    
+        if(isset($data['customer_id']))
+        {
+            $dataKendaraan['customer_id'] = $data['customer_id'];
+        }
+    
+        $vehicle = $db->insert('ws_customer_vehicles', $dataKendaraan);
+    
+        $data['vehicle_id'] = $vehicle->id;
     }
 
-    $vehicle = $db->insert('ws_customer_vehicles', $dataKendaraan);
-
-    $data['vehicle_id'] = $vehicle->id;
 } catch (\Throwable $th) {
     //throw $th;
     // unset($data['vehicle_id']);
